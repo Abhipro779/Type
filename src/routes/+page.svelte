bb2efec34748004d1601bc204d5f1e0a3834f90e
Abhi <qwertyrty34343@gmail.com>
2023-07-04 08:00:31 +0545

    First Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+import {keylist} from "./stores.js";
+</script>
+<div>This is just for testing.{#each $keylist as key, i}{key}{/each}</div>
+
---
+++ src/routes/key.svelte
@@ -0,0 +1,12 @@
+<script>
+    let size={height:10,width:10};
+    let mode;
+    let position={x:0,y:0};
+    let color=[];
+
+</script>
+<svg>
+    <rect></rect>
+    <text></text>
+    <text></text>
+</svg>
